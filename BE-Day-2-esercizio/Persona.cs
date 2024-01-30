a5b995e8691aea8602afa0cce0bec6349bddfa1d
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-01-30 13:55:40 +0100

    Aggiungere i file di progetto.

---
+++ BE-Day-2-esercizio/Persona.cs
@@ -0,0 +1,21 @@
+﻿namespace BE_Day_2_esercizio
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+        public Persona(string nome, string cognome, int eta)
+        {
+            Nome = nome;
+            Cognome = cognome;
+            Eta = eta;
+        }
+        public string getDettagli()
+        {
+            return "Nome: " + Nome + " Cognome: " + Cognome + " Eta: " + Eta;
+        }
+
+    }
+
+}
---
+++ BE-Day-2-esercizio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BE_Day_2_esercizio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il tuo nome: ");
+            string inputName = Console.ReadLine();
+            Console.WriteLine("\nInserisci il tuo cognome: ");
+            string inputCognome = Console.ReadLine();
+            Console.WriteLine("\nInserisci la tua età: ");
+            int inputEta = Convert.ToInt16(Console.ReadLine());
+
+            Persona Hachim = new Persona(inputName, inputCognome, inputEta);
+
+            Console.WriteLine(Hachim.getDettagli());
+            Console.ReadKey();
+        }
+    }
+}
